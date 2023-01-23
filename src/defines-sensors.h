880fe5bc0e6aceb27a906949255f69159c55e822
syuchan1005 <syuchan.dev@gmail.com>
2023-01-23 18:37:48 +0900

    Add sample setting

--- src/defines-sensors.h
+++ src/defines-sensors.h
@@ -7,8 +7,12 @@
 
 using namespace SlimeVR::Sensors;
 
+// NOTE: This is a sample settings. Please edit it when you using this branch.
 const std::vector<SlimeVR::Sensors::SensorInfo> SENSOR_INFOS = {
-    SensorInfo(IMU_BMI160, DEG_180, 23, new ToggleSensor::TCA9548A(TCA9548A_ADDR, 0, IMU_PORT_1)),
-    SensorInfo(IMU_BMI160, DEG_0, 23, new ToggleSensor::TCA9548A(TCA9548A_ADDR, 1, IMU_PORT_1)),
-    SensorInfo(IMU_BMI160, DEG_0, 23, new ToggleSensor::TCA9548A(TCA9548A_ADDR, 1, IMU_PORT_2)),
-    };
+    SensorInfo(IMU_BMI160, DEG_180, 23, new ToggleSensor::TCA9548A(TCA9548A_ADDR, 0, IMU_PORT_2)), // weist
+    SensorInfo(IMU_BMI160, DEG_270, 23, new ToggleSensor::TCA9548A(TCA9548A_ADDR, 5, IMU_PORT_1)), // chest
+    SensorInfo(IMU_BMI160, DEG_90, 23, new ToggleSensor::TCA9548A(TCA9548A_ADDR, 1, IMU_PORT_1)), // right - upper
+    SensorInfo(IMU_BMI160, DEG_180, 23, new ToggleSensor::TCA9548A(TCA9548A_ADDR, 1, IMU_PORT_2)), // right - lower
+    SensorInfo(IMU_BMI160, DEG_90, 23, new ToggleSensor::TCA9548A(TCA9548A_ADDR, 2, IMU_PORT_1)), // left - upper
+    SensorInfo(IMU_BMI160, DEG_180, 23, new ToggleSensor::TCA9548A(TCA9548A_ADDR, 2, IMU_PORT_2)), // left - lower
+};
